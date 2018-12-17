91c276df1ab19d29acca620a6ee86f8f9816c1f1
KrsticM <krsticm1996@gmail.com>
2018-12-18 00:52:58 +0100

    Povezivanje front i backa. Uspešno.

--- server-app/src/main/java/isa/project/controller/hotel/HotelController.java
+++ server-app/src/main/java/isa/project/controller/hotel/HotelController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,7 @@ import isa.project.dto.hotel.HotelDTO;
 import isa.project.model.hotel.Hotel;
 import isa.project.service.hotel.HotelService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping(value="server/hotels")
 public class HotelController {
--- server-app/src/main/java/isa/project/repository/hotel/HotelRepository.java
+++ server-app/src/main/java/isa/project/repository/hotel/HotelRepository.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import isa.project.model.hotel.Hotel;
 
+@CrossOrigin(origins="http://localhost:4200")
 public interface HotelRepository extends CrudRepository<Hotel, Integer> {
 
 }
