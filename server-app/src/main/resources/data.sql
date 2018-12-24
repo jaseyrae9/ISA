fbb183ecde9abc9eee22d044094380f086582dae
KrsticM <krsticm1996@gmail.com>
2018-12-24 14:23:01 +0100

    Ucitavanje data.sql-a automatski

--- server-app/src/main/resources/data.sql
+++ server-app/src/main/resources/data.sql
@@ -1,3 +1,1 @@
-insert into avio_company (avio_company_name, avio_company_description) values ('Kompanija 1', 'Neki opis');
-insert into avio_company (avio_company_name, avio_company_description) values ('Kompanija 2', 'Neki opis');
-insert into hotel (hotel_name, hotel_description) values ('Hotel proba', 'Opis');
+insert into hotel (id, hotel_name, hotel_description) values (1, 'Hotel proba', 'Opis');
