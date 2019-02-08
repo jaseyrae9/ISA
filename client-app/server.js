0f68849d2dcee458250f07adbee6d373e84914d5
milicat228 <milicat228@gmail.com>
2019-02-08 03:13:44 +0100

    Update server.js

--- client-app/server.js
+++ client-app/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ticket-reservation21'));
+app.use(express.static(__dirname + '/dist/client-app'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/ticket-reservation21/index.html'));
+res.sendFile(path.join(__dirname+'/dist/client-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
