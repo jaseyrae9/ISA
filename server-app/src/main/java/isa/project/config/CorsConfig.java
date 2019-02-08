5f6918b39fd8854b720d4a6584cd9a4c03af65e4
milicat228 <milicat228@gmail.com>
2019-02-08 03:50:59 +0100

    promena

--- server-app/src/main/java/isa/project/config/CorsConfig.java
+++ server-app/src/main/java/isa/project/config/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+		registry.addMapping("/**").allowedOrigins("https://ticket-reservation21.herokuapp.com");
 	}
 }
